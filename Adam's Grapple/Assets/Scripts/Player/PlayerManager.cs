85f82796f1cb62bc50a01831bc3b8e560a95c88a
Hadi Al Haj (RIT Student) <hba7654@rit.edu>
2023-09-12 13:26:50 -0400

    Player-platform interaction started
    Clicking on/towards a platform will trigger a message in the console

--- Adam's Grapple/Assets/Scripts/Player/PlayerManager.cs
+++ Adam's Grapple/Assets/Scripts/Player/PlayerManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
+    private Vector2 mousePosition;
+
+    [SerializeField] private float ropeLength;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,36 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void OnGrapple(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            Debug.Log("Start Grapple");
+            mousePosition = GetMousePosition();
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, GetMouseVector(), ropeLength, 1 << LayerMask.NameToLayer("Platform"));
+
+            if(hit.collider != null) 
+            {
+                Debug.Log("GRAPPLIN");
+            }
+        }
+    }
+
+    //Get the direction vector from player to mouse position
+    public Vector2 GetMouseVector()
+    {
+        Vector3 playerPos = transform.position;
+        return new Vector2(mousePosition.x - playerPos.x, mousePosition.y - playerPos.y).normalized;
+    }
+
+    //get mouse position on the screen
+    public Vector2 GetMousePosition()
+    {
+        Vector3 playerPos = transform.position;
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        Vector3 Worldpos = Camera.main.ScreenToWorldPoint(mousePos);
+        return new Vector2(Worldpos.x, Worldpos.y);
     }
 }
