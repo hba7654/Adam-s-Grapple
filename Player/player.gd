2dd3c7909a979bf8a45c1ecc3d6d95861d4abf4f
hba7654 <54544232+hba7654@users.noreply.github.com>
2023-09-25 14:19:31 -0400

    Player can shoot hook now

---
+++ Player/player.gd
@@ -0,0 +1,93 @@
+#extends CharacterBody2D
+#
+#
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+## Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#
+#
+#func _physics_process(delta):
+#	# Add the gravity.
+#	if not is_on_floor():
+#		velocity.y += gravity * delta
+#
+#	# Handle Jump.
+#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+#
+#	# Get the input direction and handle the movement/deceleration.
+#	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+#	move_and_slide()
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var hookDirVector : Vector2
+var shotHook : bool
+const hookPath = preload("res://Prefabs/hook.tscn")
+@export var maxHookPower : float
+@export var hookPowerMult : float
+
+func _ready():
+	shotHook = false
+
+
+func _physics_process(delta):
+	#Gravity!
+	#velocity = Velocity
+
+	#Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	#Velocity = velocity
+	move_and_slide()
+
+	
+	#Grappling
+	if(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		#The frame the mouse was clicked, shoot the hook
+		if (!shotHook):
+			shoot()
+		#else if hook landed
+			#allow player movement
+	else:
+		#The frame the mouse was released
+		if (shotHook):
+			shotHook = false
+
+			#Release rope
+				#Detach player from rope
+
+			#Disable movement (no rope to retract/expand)
+
+func shoot():
+	shotHook = true
+	
+	var mouse = get_local_mouse_position()
+	var hookPower = mouse.length()
+	print("Mouse distance away is: " + str(hookPower))
+
+	hookDirVector = mouse.normalized()
+	hookPower *= hookPowerMult
+	print("Hook power is: " + str(hookPower))
+
+	if (hookPower > maxHookPower):
+		hookPower = maxHookPower
+		print("Hook power is now: " + str(hookPower))
+
+	var hook = hookPath.instantiate()
+	add_child(hook);
+	hook.velocity = hookDirVector
+	hook.speed = hookPower
+
+	#TODO: - Create hook instance at player position
+	#		- add force with magnitude hookPower, direction hookDirVector
---
+++ Scripts/hook.gd
@@ -0,0 +1,39 @@
+extends RigidBody2D
+
+var velocity : Vector2
+var speed : float
+var landed : bool
+@export var bounceDamper : float
+@export var bouncePower : float
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	landed = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	#velocity.y += gravity*delta
+	if not landed:
+		var colInfo = move_and_collide(velocity.normalized() * delta * speed)
+		if !colInfo: return
+		
+		var normal = colInfo.get_normal()
+		var angle = abs(normal.angle())
+		print("Normal: " + str(normal) + " Angle: " + str(angle) + 
+		" \nAngle To Up: " + str(abs(normal.angle_to(Vector2.UP))) + " Angle To Down: " + str(abs(normal.angle_to(Vector2.DOWN))))
+		#only bounce the hook off the wall if it hits the sides
+		if(abs(normal.angle_to(Vector2.UP)) < 0.1):
+			print("landed on solid ground")
+			velocity = Vector2.ZERO
+			landed = true
+		else: if (abs(normal.angle_to(Vector2.DOWN)) < 0.1):
+			print("hit a ceiling")
+			velocity.x = 0.001
+			velocity.y = 0
+		else:
+			velocity = velocity.bounce(colInfo.get_normal().normalized())
+			print("here")
+			velocity.x *= bouncePower
+			velocity.y += bouncePower
