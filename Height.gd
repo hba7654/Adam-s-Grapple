9b7ad6f7360d1ec09f9fc69b0709f7c4b993c221
Jack Kalina (RIT Student) <jjk1280@rit.edu>
2023-09-26 12:44:54 -0400

    cleaning

--- Height.gd
+++ Height.gd
@@ -4,11 +4,9 @@ var maxHeight = 0.0
 var currentHeight = 0.0
 var heightString = ""
 var heightStringFormat = "%.2f / %.2f" 
-#todo: need to get reference to player and then grab its y from its transform
-#	actually - maybe not. if the position of the height is always linked to the player, then you can find the offset between the height node and the player
-#	and make it work. going to have to be relatively careful with this as UI changes could make this break if we go this route and we aren't paying attention 
-#also need to figure out what the "starting" y is to base all the numbers around (this may need to wait until level is started/done)
-@export var playerNode : Node
+@export var playerNode : Node #this should be the physics body for the character. not the sprite or collision shape
+
+# need to figure out what the "starting" y is to base all the numbers around (this may need to wait until level is started/done)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,7 +14,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	currentHeight = playerNode.get_position().y * -1
 	print (currentHeight)
 	if (currentHeight > maxHeight):
