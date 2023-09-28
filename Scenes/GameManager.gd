47b520dd56b1534e3f0c6b6d19b2bc1bd40f5265
Jack Kalina (RIT Student) <jjk1280@rit.edu>
2023-09-28 12:42:01 -0400

    pause done

---
+++ Scenes/GameManager.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var pauseMenu : Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if event.is_action_pressed("esc_pressed"):
+		get_tree().paused = !get_tree().paused
+		if get_tree().paused == true:
+			pauseMenu.show()
+		else:
+			pauseMenu.hide()
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
