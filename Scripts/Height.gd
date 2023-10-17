14ff5c6fce449a60f56bd31f90d8afa3d316d00c
Jack Kalina (RIT Student) <jjk1280@rit.edu>
2023-10-17 13:23:08 -0400

    numbers despise me

--- Scripts/Height.gd
+++ Scripts/Height.gd
@@ -4,7 +4,7 @@ var maxHeight = 0.0
 var currentHeight = 0.0
 var heightString = ""
 var heightStringFormat = "%.2f / %.2f" 
-var startHeight = -208.25
+var startHeight = -209.26
 @export var playerNode : Node #this should be the physics body for the character. not the sprite or collision shape
 
 # need to figure out what the "starting" y is to base all the numbers around (this may need to wait until level is started/done)
@@ -18,7 +18,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	currentHeight = (playerNode.get_position().y * -1) - startHeight
-	print (currentHeight - startHeight)
+	#print (currentHeight - startHeight)
 	if (currentHeight > maxHeight):
 		maxHeight = currentHeight
 	heightString = heightStringFormat % [currentHeight, maxHeight]
