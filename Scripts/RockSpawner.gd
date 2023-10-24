846e46a3bf39844b7a1df1d082f9c1fecda13c70
Jack Kalina (RIT Student) <jjk1280@rit.edu>
2023-10-24 13:13:05 -0400

    rock fix

--- Scripts/RockSpawner.gd
+++ Scripts/RockSpawner.gd
@@ -2,13 +2,13 @@ extends Node2D
 
 signal spawn(rock, location)
 
-var FallingRock = preload("res://Prefabs/falling_rock.tscn")
+var fallingRock = load("res://Prefabs/falling_rock.tscn")
 @export var interval : float
 var currentTime : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if interval == null:
+	if interval == 0:
 		interval = 3.0
 	currentTime = interval
 	pass # Replace with function body.
@@ -19,6 +19,10 @@ func _process(delta):
 	currentTime += delta
 	if (currentTime >= interval):
 		#spawn rock
-		
+		print("spawned rock")
+		var newRock = fallingRock.instantiate()
+		newRock.global_position = global_position
+		print(newRock.get_position())
+		add_child(newRock)
 		currentTime = 0
 	pass
