77c6eed2eea9b69bb2fd97a5e9f18c6f61b1d682
Hadi Al Haj (RIT Student) <hba7654@rit.edu>
2023-11-06 17:54:31 -0500

    A bit of reorganization

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-const hookPath = preload("res://Prefabs/Rope/hook.tscn")
+const hookPath = preload("res://Prefabs/hook.tscn")
 
 var hookDirVector : Vector2
 var shotHook : bool
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/other-test.tscn") 
+	get_tree().change_scene_to_file("res://Scenes/main-game.tscn") 
 
 
 func _on_options_pressed():
