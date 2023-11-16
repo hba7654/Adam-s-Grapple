e7120afe70e7767254a1ee8a7d0e28ecaeff4df2
Jack Kalina (RIT Student) <jjk1280@rit.edu>
2023-11-16 13:15:02 -0500

    height fix yeehaw

--- Scripts/Height.gd
+++ Scripts/Height.gd
@@ -4,20 +4,21 @@ var maxHeight = 0.0
 var currentHeight = 0.0
 var heightString = ""
 var heightStringFormat = "%.2f / %.2f" 
-var startHeight = -209.26
+var startHeight = 0.0
+@export var startHeightMarker : Node
 @export var playerNode : Node #this should be the physics body for the character. not the sprite or collision shape
 
 # need to figure out what the "starting" y is to base all the numbers around (this may need to wait until level is started/done)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#startHeight = playerNode.get_position().y * -1
+	startHeight = startHeightMarker.get_position().y * -1
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	currentHeight = (playerNode.get_position().y * -1) - startHeight
+	currentHeight = ((playerNode.get_position().y * -1) - startHeight) / 10.0
 	#print (currentHeight - startHeight)
 	if (currentHeight > maxHeight):
 		maxHeight = currentHeight
